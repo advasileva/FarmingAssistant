e3623a88617f1ba2843f0830bedc4020390b00ae
AlenaVasileva <Alenadvasileva@yandex.ru>
2022-03-15 16:30:15 +0300

    weather page fixed

--- MobileApp/App1.Android/obj/Debug/120/android/environment.armeabi-v7a.s
+++ MobileApp/App1.Android/obj/Debug/120/android/environment.armeabi-v7a.s
@@ -99,7 +99,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"9a1c6a6e-28c1-432b-9696-530bd11a9601"
+	.asciz	"7c491329-a464-49d1-9f8a-fbffe9aa6246"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
